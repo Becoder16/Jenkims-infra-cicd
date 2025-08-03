b9f24400bf884ff5478643263f19f65285de966f
Becoder16 <138563454+Becoder16@users.noreply.github.com>
2025-08-03 16:10:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,42 @@ lifecycle {
   }
 }
 
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # For SSH from anywhere - restrict if needed
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # HTTP access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "EC2_Security_Group"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
 
 
 
